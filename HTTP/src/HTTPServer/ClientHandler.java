97403855ad56868aefb2fa79c601a686db171b44
aks248 <35547351+aks248@users.noreply.github.com>
2018-02-15 21:28:49 -0500

    Implemented file reading functionality into handleGetRequest() method
    Reads the contents of a plain text file defined in a constant, and sends it to a client program through a socket.

--- HTTP/src/HTTPServer/ClientHandler.java
+++ HTTP/src/HTTPServer/ClientHandler.java
@@ -4,97 +4,115 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.FileReader;
 import java.net.Socket;
 import java.util.StringTokenizer;
 
 public class ClientHandler implements Runnable {
 
-   private final Socket socket;
+    private final Socket socket;
+    private final String diary = "src/HTTPServer/diary.txt";
+    private final String failMessage = "Failed to read diary";
 
-   public ClientHandler(Socket socket) {
-      this.socket = socket;
-   }
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
 
-   @Override
-   public void run() {
-      System.out.println("\nClientHandler Started for "
-              + this.socket);
-      handleRequest(this.socket);
-      System.out.println("ClientHandler Terminated for "
-              + this.socket + "\n");
-   }
+    @Override
+    public void run() {
+        System.out.println("\nClientHandler Started for "
+                + this.socket);
+        handleRequest(this.socket);
+        System.out.println("ClientHandler Terminated for "
+                + this.socket + "\n");
+    }
 
-   public void handleRequest(Socket socket) {
-      try (BufferedReader in = new BufferedReader(
-              new InputStreamReader(socket.getInputStream()));) {
-         String headerLine = in.readLine();
-         StringTokenizer tokenizer
-                 = new StringTokenizer(headerLine);
+    public void handleRequest(Socket socket) {
+        try (BufferedReader in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));) {
+            String headerLine = in.readLine();
+            StringTokenizer tokenizer
+                    = new StringTokenizer(headerLine);
 
-         String httpMethod = tokenizer.nextToken();
-         
-         if (httpMethod.equals("GET")) {
-            handleGetRequest(tokenizer);
-         } else if (httpMethod.equals("POST")) {
-            handlePostRequest(tokenizer);
-         } else {
-            System.out.println("The HTTP method is not recognized");
-            sendResponse(socket, 405, "Method Not Allowed");
-         }
+            String httpMethod = tokenizer.nextToken();
 
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
+            if (httpMethod.equals("GET")) {
+                handleGetRequest(tokenizer);
+            } else if (httpMethod.equals("POST")) {
+                handlePostRequest(tokenizer);
+            } else {
+                System.out.println("The HTTP method is not recognized");
+                sendResponse(socket, 405, "Method Not Allowed");
+            }
 
-   }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-   public void sendResponse(Socket socket,
-           int statusCode, String responseString) {
-      String statusLine;
-      String serverHeader = "Server: WebServer\r\n";
-      String contentTypeHeader = "Content-Type: text/html\r\n";
+    }
 
-      try (DataOutputStream out
-              = new DataOutputStream(socket.getOutputStream());) {
-         if (statusCode == 200) {
-            statusLine = "HTTP/1.0 200 OK" + "\r\n";
-            String contentLengthHeader = "Content-Length: "
-                    + responseString.length() + "\r\n";
+    public void sendResponse(Socket socket,
+            int statusCode, String responseString) {
+        String statusLine;
+        String serverHeader = "Server: WebServer\r\n";
+        String contentTypeHeader = "Content-Type: text/html\r\n";
 
-            out.writeBytes(statusLine);
-            out.writeBytes(serverHeader);
-            out.writeBytes(contentTypeHeader);
-            out.writeBytes(contentLengthHeader);
-            out.writeBytes("\r\n");
-            out.writeBytes(responseString);
-         } else if (statusCode == 405) {
-            statusLine = "HTTP/1.0 405 Method Not Allowed" + "\r\n";
-            out.writeBytes(statusLine);
-            out.writeBytes("\r\n");
-         } else {
-            statusLine = "HTTP/1.0 404 Not Found" + "\r\n";
-            out.writeBytes(statusLine);
-            out.writeBytes("\r\n");
-         }
+        try (DataOutputStream out
+                = new DataOutputStream(socket.getOutputStream());) {
+            if (statusCode == 200) {
+                statusLine = "HTTP/1.0 200 OK" + "\r\n";
+                String contentLengthHeader = "Content-Length: "
+                        + responseString.length() + "\r\n";
 
-         out.close();
-      } catch (IOException ex) {
-         // Handle exception
-      }
-   }
+                out.writeBytes(statusLine);
+                out.writeBytes(serverHeader);
+                out.writeBytes(contentTypeHeader);
+                out.writeBytes(contentLengthHeader);
+                out.writeBytes("\r\n");
+                out.writeBytes(responseString);
+            } else if (statusCode == 405) {
+                statusLine = "HTTP/1.0 405 Method Not Allowed" + "\r\n";
+                out.writeBytes(statusLine);
+                out.writeBytes("\r\n");
+            } else {
+                statusLine = "HTTP/1.0 404 Not Found" + "\r\n";
+                out.writeBytes(statusLine);
+                out.writeBytes("\r\n");
+            }
 
-   private void handleGetRequest(StringTokenizer tokenizer) {
-      System.out.println("Get method processed");
-      String httpQueryString = tokenizer.nextToken();
-      StringBuilder responseBuffer = new StringBuilder();
-      responseBuffer
-              .append("<html><h1>WebServer Home Page.... </h1><br>")
-              .append("<b>Welcome to my web server!</b><BR>")
-              .append("</html>");
-      sendResponse(socket, 200, responseBuffer.toString());
-   }
+            out.close();
+        } catch (IOException ex) {
+            // Handle exception
+        }
+    }
 
-   private void handlePostRequest(StringTokenizer tokenizer) {
-      // TODO
-   }
+    private void handleGetRequest(StringTokenizer tokenizer) {
+        System.out.println("Get method processed");
+        String httpQueryString = tokenizer.nextToken();
+        FileReader diaryReader;
+        String line;
+        try {
+            diaryReader = new FileReader(diary);
+            BufferedReader diaryBuffer = new BufferedReader(diaryReader);
+            StringBuilder responseBuffer = new StringBuilder();
+            while((line = diaryBuffer.readLine()) != null) {
+                responseBuffer.append(line);
+                responseBuffer.append("\n");
+            }
+            if (responseBuffer.toString().equals("")) {
+                sendResponse(socket, 200, "The diary is empty");
+            }
+            else {
+                sendResponse(socket, 200, responseBuffer.toString());
+            }
+        } 
+        catch (IOException e) {
+            System.out.println(failMessage);
+            sendResponse(socket, 200, failMessage);
+        }
+    }
+
+    private void handlePostRequest(StringTokenizer tokenizer) {
+        // TODO
+    }
 }

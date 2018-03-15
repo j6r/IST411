7520733b819f379ef7a9864f5fcf9c75afc68525
Jaydeboy <jn587256@gmail.com>
2018-03-15 00:13:21 -0400

    Merge origin/master
    Conflicts:
    	src/hw02/Bank.java

---
+++ JAXRS/src/main/java/Recipes/RecipeDAO.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Recipes;
+
+import java.util.ArrayList;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+
+/**
+ *
+ * @author Single-eye
+ */
+public class RecipeDAO {
+    
+    private ArrayList<Recipe> Cookbook = new ArrayList();
+    
+
+    public void objToJSon() {
+        
+        
+        
+    }
+
+    public void displayJSon() {
+
+    }
+
+}

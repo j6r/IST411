5d3c140a609730c48974ad0c081ef0c502d4f80d
j6r <32883148+j6r@users.noreply.github.com>
2018-03-17 18:23:31 -0700

    Prototype method to create a new recipe

--- JAXRS/src/main/java/edu/psu/ist411/service/ApplicationConfig.java
+++ JAXRS/src/main/java/edu/psu/ist411/service/ApplicationConfig.java
@@ -1,28 +1,27 @@
-
 package edu.psu.ist411.service;
 
 import java.util.Set;
 import javax.ws.rs.core.Application;
 
-
 @javax.ws.rs.ApplicationPath("webresources")
 public class ApplicationConfig extends Application {
 
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> resources = new java.util.HashSet<>();
-        addRestResourceClasses(resources);
-        return resources;
-    }
+   @Override
+   public Set<Class<?>> getClasses() {
+      Set<Class<?>> resources = new java.util.HashSet<>();
+      addRestResourceClasses(resources);
+      return resources;
+   }
 
-    /**
-     * Do not modify addRestResourceClasses() method.
-     * It is automatically populated with
-     * all resources defined in the project.
-     * If required, comment out calling this method in getClasses().
-     */
-    private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(edu.psu.ist411.service.RecipeResource.class);
-    }
-    
+   /**
+    * Do not modify addRestResourceClasses() method.
+    * It is automatically populated with
+    * all resources defined in the project.
+    * If required, comment out calling this method in getClasses().
+    */
+   private void addRestResourceClasses(Set<Class<?>> resources) {
+      resources.add(edu.psu.ist411.service.NewRecipeResource.class);
+      resources.add(edu.psu.ist411.service.RecipeResource.class);
+   }
+
 }
---
+++ JAXRS/src/main/java/edu/psu/ist411/service/NewRecipeResource.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.psu.ist411.service;
+
+import edu.psu.ist411.recipe.Recipe;
+import edu.psu.ist411.recipe.RecipeDAO;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author redjen
+ */
+@Path("recipes/new")
+public class NewRecipeResource {
+
+   @Context
+   private UriInfo context;
+   
+   private static final String RETURN_TEMPLATE = "<html><head></head>"
+           + "<body><div>Your recipe was created successfully!"
+           + "To view it click <a href='/JAXRS/webresources/recipes/%d'>here</a>.";
+
+   /**
+    * Creates a new instance of NewRecipeResource
+    */
+   public NewRecipeResource() {
+   }
+
+   /**
+    * Retrieves representation of an instance of edu.psu.ist411.service.NewRecipeResource
+    * @return an instance of java.lang.String
+    */
+   @POST
+   @Produces(MediaType.TEXT_HTML)
+   public String getHtml(@FormParam("recipeName") String recipeName) {
+      String result = "";
+      Recipe recipe = null;
+      RecipeDAO dao = null;
+      HashSet<String> ingredients = new HashSet<>();
+      
+      try {
+         dao = new RecipeDAO();
+         recipe = dao.addRecipe(recipeName, ingredients);
+         result = String.format(RETURN_TEMPLATE, recipe.getRecipeID());
+         
+      } catch (IOException ex) {
+         Logger.getLogger(NewRecipeResource.class.getName()).log(Level.SEVERE, null, ex);
+      }
+      
+      return result;
+   }
+
+}
